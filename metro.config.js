ed1101f8e527f37055d4a46e18832535223f1649
alejandro.cabanas <alejandro.cabanas@bancontinental.com.py>
2026-05-21 11:43:56 -0300

    se maneja svg

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,17 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = mergeConfig(getDefaultConfig(__dirname), {});
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+const config = mergeConfig(defaultConfig, {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: [...assetExts.filter(ext => ext !== 'svg'), 'tflite'],
+    sourceExts: [...sourceExts, 'svg'],
+  },
+});
 
 module.exports = withNativeWind(config, { input: "./global.css" });
